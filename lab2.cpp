ddc9f3ab3ea60e8418e0360eb99cec128a356fd2
Liza Stadnichenko <6068011grell@gmail.com>
2024-09-20 15:53:12 +0300

    lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int number1;
+    cout << "Введіть перше число: ";
+    cin >> number1;
+    int number2;
+    cout << "Введіть друге число: ";
+    cin >> number2;
+    
+    while (number1 != number2) {
+        if (number1 > number2) {
+            number1 = number1 - number2;
+        } 
+        else {
+            number2 = number2 - number1;
+        }
+    }
+    
+    int nsk = (number1 * number2 / number1);
+    cout << nsk << " ";
+    
+    return 0;
+}
